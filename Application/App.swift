8661139ffb44619630e13c5734be489b7a9c650e
ooper-shlab <nagata@shlab.jp>
2021-03-24 17:24:41 +0900

    Added issing file

---
+++ Application/App.swift
@@ -0,0 +1,18 @@
+//
+//  App.swift
+//  CPU-GPU-Synchronization-iOS
+//
+//  Created by 開発 on 3/24/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CPUGPUSynchronization: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
